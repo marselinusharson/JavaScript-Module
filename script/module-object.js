1ea14fdaa6794af8b33c1188ba148abbb6edd1ea
marselinusharson <marselinus@undiksha.ac.id>
2022-01-18 13:00:31 +0800

    module object

---
+++ script/module-object.js
@@ -0,0 +1,13 @@
+export const firstName = "Marselinus";
+export const middleName = "Harson";
+export const lastName = "Rewo";
+
+export function hallo (name){
+    console.info(`Hello ${name}`)
+}
+
+export class Person{
+    constructor(name){
+        this.name=name
+    }
+}
